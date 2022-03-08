cdaf13b27e1122e54b4b9fb4c6f103b8c469b28a
DESKTOP-S24B0M7\gutro <gutrovr@gmail.com>
2022-03-08 16:17:51 +0500

    The text receipt code has been moved to a separate file

--- Project2/Project2/Source.h
+++ Project2/Project2/Source.h
@@ -1,2 +1,3 @@
 #pragma once
 void ReplaceSubstring(char* ptrSM, int sizeSM, char* subStr, int sizeSubstr, char* insertedStr, int sizeInsStr);
+char REPLACE(int argc, char* argv[]);
--- Project2/Project2/main.c
+++ Project2/Project2/main.c
@@ -9,69 +9,7 @@
 int main(int argc, char* argv[])
 {
 	setlocale(LC_ALL, ".1251");
-	FILE* in;
-	char *sourceString = NULL, *subStr = NULL, *insertedStr=NULL;
-	int sizeFile = 0, sizeSubstr=0, sizeInsertedStr=0, sizeSM=0, i=0, it_read=0, tmp =1;
-
-	sourceString = (char*)malloc(512 * sizeof(char));
-	
-	printf("Source file: %s\n", argv[1]);
-	printf("Source string: %s\nReplaced string: %s\n", argv[2], argv[3]);
-	
-	fopen_s(&in, argv[1], "r");
-	while (!feof(in))
-	{
-		tmp = fgetc(in);
-		sizeFile++;
-	}
-
-	insertedStr = &argv[3][0];
-	subStr = &argv[2][0];
-
-	while (1) //получить размер искомой подстроки
-	{
-		if (subStr[i] != '\0') { i++; }
-		else 
-			break;
-	}
-	sizeSubstr = i;
-	i = 0;
-	while (1) //получить размер заменяемой подстроки
-	{
-		if (insertedStr[i] != '\0') { i++; }
-		else
-			break;
-	}
-	sizeInsertedStr = i;
-
-	i = (sizeFile / 512) + 1;
-	fseek(in, 0, SEEK_SET);
-	if (sizeFile >= 512) //если файл больше 512 байт считываем блоками
-	{
-		while (i != 0)
-		{
-			it_read = 0;
-			while(it_read < 512 && !feof(in))
-			{
-				fscanf_s(in, "%c", &sourceString[it_read]);
-				it_read++;
-			}
-			i--;
-			if (i == 0) sourceString = (char*)realloc(sourceString, (it_read-1)*sizeof(char));
-			it_read--;
-			ReplaceSubstring(&sourceString[0], it_read, &subStr[0], sizeSubstr, &insertedStr[0], sizeInsertedStr); //ф-я замены подстроки
-		}
-	}
-	else
-	{
-		for (int j = 0; j < sizeFile; j++)
-			fscanf_s(in, "%c", &sourceString[j]);//если меньше считываем весь файл
-		sourceString[sizeFile - 1] = '\0';
-		sizeFile--;
-		ReplaceSubstring(&sourceString[0], sizeFile, &subStr[0], sizeSubstr, &insertedStr[0], sizeInsertedStr); //ф-я замены подстроки
-	}
-	free(sourceString);
-	fclose(in);
+	REPLACE(argc, &argv[0]);
 	system("pause");
 	return 0;
 }
