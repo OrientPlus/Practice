0dcacbf12a736761470170c894491e1655025ffc
DESKTOP-S24B0M7\gutro <gutrovr@gmail.com>
2022-04-29 21:35:56 +0500

    Sketch of the lab5(BigNum)

---
+++ Project5/Project5/bigNum.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdlib.h>
+
+
+
+int bigNum(int argc, char** argv)
+{
+	char strNum1[37], strNum2[37];
+	unsigned int num1[4], num2[4];
+
+	getNumbers(num1, num2, argc, &argv[0]);
+}
+
+void getNumbers(unsigned int* num1, unsigned int* num2, int argc, char* argv[])
+{
+	int lenNum1, lenNum2, it=0;
+	while (argv[1][it] != '\0')
+	{
+
+	}
+}
---
+++ Project5/Project5/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+	bigNum(argc, &argv[0]);
+}
